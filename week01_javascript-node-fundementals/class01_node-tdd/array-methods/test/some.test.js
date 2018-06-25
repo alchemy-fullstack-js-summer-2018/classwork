70ff4407cbdd616a87821e338c26e59d40692ed9
martypdx <marty@martypdx.com>
2018-06-25 08:52:46 -0700

    day one setup

---
+++ week01_javascript-node-fundementals/class01_node-tdd/array-methods/test/some.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const some = require('../lib/some');
+
+describe('some', () => {
+
+    describe('array method assumptions', () => {
+
+    });
+
+    // const isEven = x => x % 2 === 0;
+
+    // returns false if no elements match
+
+    // returns true if at least one element matches
+
+    // short-circuits after first true element
+});
