5e57e31ea002677f397cdcdcc99da9d9df51d728
martypdx <marty@martypdx.com>
2018-07-10 11:45:25 -0700

    setup for class 07

---
+++ week02_node-server-fundementals/class07_HTTP-REST/HTTP.md
@@ -0,0 +1,18 @@
+## Core HTTP Parts
+
+## Assignment
+
+For your groups topic, design and give a whiteboard tutorial (combination of pictorial and verbal). Include:
+
+1. Description of what it is
+1. Why is it important? (What does it control?)
+1. Show specific examples and/or tables of most common options
+
+
+## Groups
+
+1. HTTP Method (Verb)
+1. Url (Path)
+1. Query Parameters
+1. Content-Type Header
+1. HTTP Status Codes
---
+++ week02_node-server-fundementals/class07_HTTP-REST/quiz-review.js
@@ -0,0 +1,18 @@
+/*
+Write a tested (mocha unit tested) library function (a function exported from 
+a module/file) that takes an array of numbers (you can assume valid input) 
+and returns a new array that contains the square (n * n, or Math.pow(n, 2)) 
+of all of the odd input numbers, but only those squares that are greater 
+than 24.
+
+Test Case:
+
+Input	                Output
+[2, 3, 9, 12, 8, 5, 7]	[81, 25, 49]
+*/
+
+
+/* big-odd-squares.test.js */
+
+
+/* big-odd-squares.js */
