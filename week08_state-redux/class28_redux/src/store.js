aa235d29c473f260219b132c8c11bfa9c7734324
martypdx <marty@martypdx.com>
2018-08-23 08:50:49 -0700

    leave comment for switch to use counter demo

--- week08_state-redux/class28_redux/src/store.js
+++ week08_state-redux/class28_redux/src/store.js
@@ -1,8 +1,12 @@
 import { createStore } from 'redux';
 import { animals } from './components/animals/reducers';
+// switch to this import to run counter demo
+// import { count } from './components/demo/reducers';
 
 const store = createStore(
   animals,
+  // switch for counter demo
+  // count,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
